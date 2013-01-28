a8e2d1edc1c4fd56cc4eb2e2fcf4dba5ecdc2b3b
Mauro Bianchi <bianchimro@gmail.com>
2013-01-28 01:16:41 +0100

    added active_if_starts tag

--- bootstrapped/templatetags/bootstrap.py
+++ bootstrapped/templatetags/bootstrap.py
@@ -112,3 +112,10 @@ def is_checkbox(field):
 @register.filter
 def is_radio(field):
     return field.field.widget.__class__.__name__.lower() == "radioselect"
+
+
+@register.simple_tag
+def active_if_starts(path, pattern):
+    if path.startswith(pattern):
+        return 'active'
+    return ''
